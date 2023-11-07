22be9653245aabc99d1b1cba864c6e018dc4a7a8
Mateus Reginaldo <91707367+MtReginaldo@users.noreply.github.com>
2023-11-06 21:23:53 -0300

    Add files via upload

---
+++ IntervaloArvore/src/IntervaloArvore2.java
@@ -0,0 +1,61 @@
+class NodeArvore {
+    int valor;
+    NodeArvore noEsquerdo;
+    NodeArvore noDireito;
+
+    NodeArvore(int valor) {
+        this.valor = valor;
+    }
+
+    NodeArvore(int valor, NodeArvore esquerdo, NodeArvore direito) {
+        this.valor = valor;
+        this.noEsquerdo = esquerdo;
+        this.noDireito = direito;
+    }
+}
+
+public class IntervaloArvore2 {
+    private NodeArvore raiz;
+
+    public IntervaloArvore2(NodeArvore raiz) {
+        this.raiz = raiz;
+    }
+
+    public int calcularSomaIntervalo(int limiteInferior, int limiteSuperior) {
+        return calcularSomaIntervalo(raiz, limiteInferior, limiteSuperior);
+    }
+
+    private int calcularSomaIntervalo(NodeArvore no, int limiteInferior, int limiteSuperior) {
+        if (no == null) {
+            return 0;
+        }
+
+        int soma = 0;
+        if (limiteInferior <= no.valor && no.valor <= limiteSuperior) {
+            soma += no.valor;
+        }
+        if (limiteInferior < no.valor) {
+            soma += calcularSomaIntervalo(no.noEsquerdo, limiteInferior, limiteSuperior);
+        }
+        if (no.valor < limiteSuperior) {
+            soma += calcularSomaIntervalo(no.noDireito, limiteInferior, limiteSuperior);
+        }
+
+        return soma;
+    }
+
+    public static void main(String[] args) {
+        NodeArvore raiz = new NodeArvore(10);
+        raiz.noEsquerdo = new NodeArvore(5);
+        raiz.noEsquerdo.noEsquerdo = new NodeArvore(3);
+        raiz.noEsquerdo.noEsquerdo.noEsquerdo = new NodeArvore(1);
+        raiz.noEsquerdo.noDireito = new NodeArvore(7);
+        raiz.noEsquerdo.noDireito.noEsquerdo = new NodeArvore(6);
+        raiz.noDireito = new NodeArvore(15);
+        raiz.noDireito.noEsquerdo = new NodeArvore(13);
+        raiz.noDireito.noDireito = new NodeArvore(18);
+
+        IntervaloArvore2 arvore = new IntervaloArvore2(raiz);
+        System.out.println(arvore.calcularSomaIntervalo(6, 10));
+    }
+}
